f4f931845fc0fd7edec088dc8ebb872db487dd97
smock9176-hue <smock9176@gmail.com>
2026-04-17 20:40:43 +0300

    Add Pharos Atlantic contracts file

---
+++ data/pharos_atlantic_contracts.py
@@ -0,0 +1,29 @@
+# Pharos Atlantic Contracts
+
+# Token Addresses
+PHRS token address: 0x0000000000000000000000000000000000000000
+USDT address: 0xe7e84b8b4f39c507499c40b4ac199b050e2882d5
+USDC address: 0xe0be08c77f415f577a1b3a9ad7a1df1479564ec8
+WBTC address: 0x0c64f03eea5c30946d5c55b4b532d08ad74638a4
+WETH address: 0x7d211F77525ea39A0592794f793cC1036eEaccD5
+
+# Router Addresses
+Zenith Router: 0x9b84996d519c6a046da9fb6b9be41bad217cb783
+Faroswap Router: 0xf54675d60CCa5b97B697DF3c47F4f21fC2a78919
+Brokex Position Router: 0xEcbAc797f28f412ddF0D38B50f5B4a6904d46e0A
+
+# Lending
+ELFi Lending: 0x4f5b54d4AF2568cefafA73bB062e5d734b55AA05
+R2USD: 0x4f5b54d4AF2568cefafA73bB062e5d734b55AA05
+
+# Controller and Minter
+PNS Controller: 0x51be1ef20a1fd5179419738fc71d95a8b6f8a175
+NFT Minter: 0xF3bF0736DDf31da6a542c7cF97652F4D0835B9d3
+
+# Network Configuration
+Pharos Atlantic:
+Chain ID: 8008
+RPC: https://atlantic.dplabs-internal.com
+
+# Last Updated
+Timestamp: 2026-04-17 17:39:36
